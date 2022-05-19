b4390be3375c20eb9ebc95d59cca935036dd88df
SachaDulout <sachadulout@outlook.com>
2022-05-19 10:46:04 +0200

    Début création des portails

---
+++ Portail.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "mvtperso.h"
+
+
+void mouse(point& p, int& j);
+
+class Portail {
+    int h_portail=100;
+    int w_portail=5;
+    point position_portail={0,0};
+
+    Color col;
+public:
+    Portail(Color color);
+    point get_portal_position();
+    void set_portal_position(point p);
+    void Draw_portal();
+    void erase_portal();
+
+
+
+};
